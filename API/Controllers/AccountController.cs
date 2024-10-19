d58ff8e1fc7bf3a54480abe7c2bfcf4ad911d7e6
AutomaticOrca <liangjiaying1013@gmail.com>
2024-10-19 17:52:00 +1100

    set gender is required when register/login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -32,7 +32,8 @@ public class AccountController(DataContext context, ITokenService tokenService, 
         {
             Username = user.UserName,
             Token = tokenService.CreateToken(user),
-            KnownAs = user.KnownAs
+            KnownAs = user.KnownAs,
+            Gender = user.Gender
         };
         // Create an instance of HMACSHA512 to hash the password
         // using var hmac = new HMACSHA512();
@@ -84,6 +85,7 @@ public class AccountController(DataContext context, ITokenService tokenService, 
             Username = user.UserName,
             KnownAs = user.KnownAs,
             Token = tokenService.CreateToken(user),
+            Gender = user.Gender,
             PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
         };
     }
